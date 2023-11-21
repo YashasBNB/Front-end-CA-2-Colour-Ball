8e0dfaff2450a89daf6caba4157cf82c34cd39e0
YashasBNB <yashasnaidu3@gmail.com>
2023-11-21 13:29:39 +0530

    done

--- start.js
+++ start.js
@@ -1,5 +1,15 @@
-const play = document.getElementsByClassName("play-button");
+// const playButton = document.getElementById("play-button");
+// playButton.onclick = () => {
+//   // window.location.assign("./play.html");
+//   // window.location.href ="./play.html"
+//   // alert("why??")
+  
+//       // Create an anchor element
+//       const anchor = document.createElement("a");
 
-play.onclick = () => {
-  window.location.href = "play.html";
-};
+//       // Set the href attribute to the target URL
+//       anchor.href = "www.google.com";
+
+//       // Trigger a click on the anchor element
+//       anchor.click();
+// };
